250ad91a95de1a8c0056bd0fee0ffaca1eedf2d7
pechkadenikita <Admin@DESKTOP-8FUPCP7>
2025-01-11 17:07:42 +0530

    initial commit

---
+++ src/main/java/com/tka/edufuratech/api/edufuratechApplication.java
@@ -0,0 +1,14 @@
+package com.tka.edufuratech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class edufuratechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(edufuratechApplication.class, args);
+		System.err.println("the application is started..!!!!");
+	}
+
+}
